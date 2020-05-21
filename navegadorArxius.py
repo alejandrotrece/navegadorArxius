c7d766f0ff653a0e50e61866849644c9d74a2a66
alejandrotrece <alejandro.martinez@iesjoandaustria.org>
2020-05-21 13:11:42 +0000

    Fa funcions (bàsic)

--- navegadorArxius.py
+++ navegadorArxius.py
@@ -1,4 +1,6 @@
 import os, sys
+import shutil
+from shutil import copyfile
 
 path = "fitxers"
 dirs = os.listdir( path )
@@ -8,6 +10,20 @@ for file in dirs:
 opcio = input("Quin arxiu vols?")
 if os.path.exists(path+"/"+opcio):
   fitxer = open(path+"/"+opcio, "r")
-  print(fitxer)
+  llegir = fitxer.read()
+  print(llegir)
 else:
   print("L'arxiu demanat no existeix")
+  
+if llegir[0] == "moure":
+  if os.path.exists():
+    os.rename(llegir[1], llegir[2])
+  else:
+    print("L'arxiu demanat no existeix")
+elif llegir[0] == "borra":
+  os.remove(llegir[1])
+elif llegir[0] == "copia":
+  copyfile("fitxers/a.txt", "fitxers/x.txt")
+
+
+
